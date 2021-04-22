d62146db474dd63f6f9b6b3512901d873dcf8446
miguelrisquelme <miguelrisquelme@gmail.com>
2021-04-22 12:18:26 -0300

    criando contextos do player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import { api } from "./../services/api";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 import styles from "./home.module.scss";
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/PlayerContexts";
 
 type Episode = {
 	id: string;
@@ -26,6 +28,8 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }) {
+	const { play } = useContext(PlayerContext);
+
 	return (
 		<div className={styles.homepage}>
 			<section className={styles.lastestEpisodes}>
@@ -51,7 +55,7 @@ export default function Home({ latestEpisodes, allEpisodes }) {
 									<span>{episode.duration}</span>
 								</div>
 
-								<button>
+								<button type="button" onClick={() => play(episode)}>
 									<img src="./play-green.svg" alt="Tocar Episódio" />
 								</button>
 							</li>
